fd316eeb885caa04b8ec31338c92bfdea5d06f22
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2024-04-18 16:26:23 -0300

    .

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -37,12 +37,18 @@ class MarcaController extends Controller
      */
     public function store(Request $request)
     {
-        //$marca = Marca::create($request->all());
 
         $request ->validate($this->marca->rules(),$this->marca->feedback());
-
         //foi instanciado o objeto mais e aqui ele esta sendo chamdo para criar um registro com todos os request
-        $marca = $this->marca->create($request->all());
+       // $marca = $this->marca->create($request->all());
+
+       //acessar os input nome
+       //dd($request->nome);
+       //dd($request->get('nome'));
+
+
+       //dd($request->file('imagem'));
+       dd($request->imagem);
 
         //retorna um json com status code 201
         return response()->json($marca, 201);
