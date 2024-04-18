57e0b412d30f796c31a8d51014472e334e81f87d
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2024-04-18 11:40:05 -0300

    .

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -55,7 +55,12 @@ class MarcaController extends Controller
      */
     public function update(Request $request, Marca $marca)
     {
-        //
+        // print_r($request->all());//os dados atualizados
+        // echo '<hr>';
+        // print_r($marca->getAttributes());//os dados antigos
+
+        $marca->update($request->all());
+        return $marca;
     }
 
     /**
