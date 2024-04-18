670e5210864c86a5bc95f62298a94fcb15652b17
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2024-04-18 12:44:44 -0300

    .

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -7,13 +7,20 @@ use Illuminate\Http\Request;
 
 class MarcaController extends Controller
 {
+    protected $marca;
+
+    public function __construct(Marca $marca)
+    {
+        $this->marca = $marca;
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        $marcas = Marca::all();
-        return $marcas;
+        //$marcas = Marca::all();
+        $marca = $this->marca->all();
+        return $marca;
     }
 
     /**
@@ -29,19 +36,22 @@ class MarcaController extends Controller
      */
     public function store(Request $request)
     {
-        $marca = Marca::create($request->all());
+        //$marca = Marca::create($request->all());
+        $marca = $this->marca->create($request->all());
         return $marca;
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Marca $marca)
+    public function show( $id)
     {
+        $marca = $this->marca->find($id);
+        if ($marca === null) {
+            return ['erro'=>'Recurso indisponivel - (ver id)'];
+        }
         return $marca;
-        echo'Chegamos até aqui (show)';
     }
-
     /**
      * Show the form for editing the specified resource.
      */
@@ -53,12 +63,12 @@ class MarcaController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Marca $marca)
+    public function update(Request $request, $id)
     {
-        // print_r($request->all());//os dados atualizados
-        // echo '<hr>';
-        // print_r($marca->getAttributes());//os dados antigos
-
+        $marca = $this->marca->find($id);
+        if ($marca === null) {
+            return ['erro'=>'Recurso indisponivel - (Atualização)'];
+        }
         $marca->update($request->all());
         return $marca;
     }
@@ -66,9 +76,13 @@ class MarcaController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Marca $marca)
+    public function destroy($id)
     {
+       $marca = $this->marca->find($id);
+       if ($marca === null) {
+        return ['erro'=>'Recurso indisponivel - (Exclusão)'];
+    }
        $marca->delete();
-       return ['msg'=>'A marca foi deletado'];
+       return ['msg'=>'A marca foi removida'];
     }
 }
