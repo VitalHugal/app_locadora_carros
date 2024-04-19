b852c17f001be2c734e7c1d1bbb0425685b8a13d
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2024-04-19 11:04:41 -0300

    .

--- app/Models/Modelo.php
+++ app/Models/Modelo.php
@@ -8,13 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Modelo extends Model
 {
     use HasFactory;
-    protected $fillable = ['marca_id', 'nome', 'iamgem', 'numero_portas', 'lugares', 'air_bag', 'abs'];
+    protected $fillable = ['marca_id', 'nome', 'imagem', 'numero_portas', 'lugares', 'air_bag', 'abs'];
 
     public function rules()
     {
         return [
             'marca_id' =>'exists:marcas,id',
-            'nome' => 'required|unique:modleos,nome,' . $this->id . '|min:3',
+            'nome' => 'required|unique:modelos,nome,' . $this->id . '|min:3',
             'imagem' => 'required|file|mimes:png, jpg,jpeg',
             'numero_portas'=> 'required|integer|digits_between:1,5',//1 a 5
             'lugares'=> 'required|integer|digits_between:1,20',//1 a 20
