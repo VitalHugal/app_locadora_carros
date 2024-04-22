6d3a82313cdd45eb771f7da99c205217d9575270
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2024-04-22 16:34:04 -0300

    LocacoesRepository/LocacoesController;/Locacoes

--- app/Models/Locacao.php
+++ app/Models/Locacao.php
@@ -7,5 +7,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Locacao extends Model
 {
-    protected  $table ='locacoes';
+    use HasFactory;
+    protected $table = 'locacoes';
+    protected $fillable = [
+        'cliente_id',
+        'carro_id',
+        'data_inicio_periodo',
+        'data_final_previsto_periodo',
+        'data_final_realizado_periodo',
+        'valor_diaria',
+        'km_inicial',
+        'km_final'
+    ];
+
+    public function rules()
+    {
+        return [
+            'cliente_id' => 'required',
+            'carro_id' => 'required',
+            'data_inicio_periodo' => 'required',
+            'data_final_previsto_periodo' => 'required',
+            'data_final_realizado_periodo' => '',
+            'valor_diaria' => 'required',
+            'km_inicial' => 'required',
+            'km_final' => '',
+        ];
+    }
 }
