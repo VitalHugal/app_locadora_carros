6dd38af91d9525a8437726b73a63ac3ad30a9307
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2024-04-22 17:13:14 -0300

    Configurações do JWT

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,11 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'api' =>[
+            'driver' => 'jwt',
+            'provider' => 'users',
+            'hash' => false,
+        ],
     ],
 
     /*
@@ -64,6 +69,7 @@ return [
             'driver' => 'eloquent',
             'model' => env('AUTH_MODEL', App\Models\User::class),
         ],
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
 
         // 'users' => [
         //     'driver' => 'database',
