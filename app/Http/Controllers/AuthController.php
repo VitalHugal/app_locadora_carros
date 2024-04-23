9cdb9e9b2e703907370c5317bec2deae110c2fe5
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2024-04-23 10:19:52 -0300

    Autenticação do login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,7 +6,17 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login(){
+    public function login(Request $request){
+
+        $credenciais = $request->all(['email','password']); //autenticação do usuario (email e senha)
+
+        $token = auth('api')->attempt($credenciais);//retornar um tokem jwt
+        //dd($token);
+        if($token){ //usuario autenticado com sucesso
+            return response()->json(['token'=> $token]);
+        }else{// erro de autenticação (senha ou usuario)
+            return response()->json(['erro'=>'Usuário ou senha inválido!'], 403); 
+        }
         return 'login';
     }
     public function logout(){
